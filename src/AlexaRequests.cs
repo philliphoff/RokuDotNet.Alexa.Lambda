7db0bb25df8d29d60f3b712dd85a7472e6169083
Phillip Hoff <phillip@orst.edu>
2018-12-18 15:14:45 -0800

    Initial commit.

---
+++ src/AlexaRequests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+
+namespace RokuDotNet.Alexa.Lambda
+{
+    public class AlexaRequest
+    {
+    }
+
+    public class AlexaResponse
+    {
+    }
+
+    public static class AlexaRequests
+    {
+        [LambdaSerializer(typeof(JsonSerializer))]
+        public static Task<AlexaResponse> TestRequestAsync(AlexaRequest request)
+        {
+            return Task.FromResult(new AlexaResponse());
+        }
+    }
+}
